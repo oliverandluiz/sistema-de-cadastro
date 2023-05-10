8897494497ac231313220cdf56b7b8dfed610534
Andre Oliveira <oliverandluiz@outlook.com>
2023-05-10 16:21:32 -0300

    Andre realizou esse commit de teste

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
+// mais um console log teste:
+console.log("Andre fez mais este commit")
