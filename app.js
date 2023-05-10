fe6e107937832bba75454b1efbcd9ece8c19b0f3
Andre Oliveira <oliverandluiz@outlook.com>
2023-05-10 16:49:55 -0300

    Atualizando js

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esta se conectando ao banco de dados")
 // mais um console log teste:
 console.log("Andre fez mais este commit")
-console.log("Enviando para o GitHub")
